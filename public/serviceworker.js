63cd3ae652e140420e0e177ff25504fba11f2fe5
quewon <36914642+quewon@users.noreply.github.com>
2024-11-30 18:14:01 +0900

    version

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,7 +1,7 @@
 // should be versioned to update cached resources
 
-const static_cache_name = 'site-static-v1-2-1';
-const dynamic_cache_name = 'site-dynamic-v1-2-1';
+const static_cache_name = 'site-static-v1-2-3';
+const dynamic_cache_name = 'site-dynamic-v1-2-3';
 const dynamic_cache_limit = 50;
 
 const static_assets = [
